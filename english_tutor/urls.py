e8db46fab3d00ca7bf07ce0b49f90eb8411c49a9
Georgii <67765117+Vandyshge@users.noreply.github.com>
2025-04-15 00:07:03 +0300

    Добавил Удаление уроков и карточек. Добавил тренировку по урокам

--- english_tutor/urls.py
+++ english_tutor/urls.py
@@ -21,11 +21,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('cards/', views.card_list, name='card_list'),
     path('cards/add/', views.add_card, name='add_card'),
+    path('cards/delete/<int:pk>/', views.delete_card, name='delete_card'),
     path('lessons/', views.lesson_list, name='lesson_list'),
     path('lessons/add/', views.add_lesson, name='add_lesson'),
+    path('lessons/delete/<int:pk>/', views.delete_lesson, name='delete_lesson'),
+    path('lessons/<int:pk>/cards/', views.lesson_cards, name='lesson_cards'),
     path('practice/', views.practice, name='practice'),
+    path('practice/lesson/<int:pk>/', views.practice_lesson, name='practice_lesson'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
